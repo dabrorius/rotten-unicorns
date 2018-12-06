6c1863b65c61c27bcb77b673c6608c0be29ca922
Filip Defar <filip.defar@toptal.com>
2018-12-06 11:37:08 +0100

    Compile project to /docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -18,12 +19,15 @@ module.exports = {
     ]
   },
   output: {
-    path: path.resolve(__dirname)
+    path: path.resolve(__dirname, "docs")
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "src/index.html" }),
     new MiniCssExtractPlugin({
       filename: "style.[hash].css"
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: "src/images/*", to: "./images/", flatten: true }
+    ])
   ]
 };
